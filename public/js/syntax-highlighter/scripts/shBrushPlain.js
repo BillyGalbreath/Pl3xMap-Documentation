acd037293e0fb5b15d539713e9e361280c7f28b1
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-05-22 07:36:03 -0500

    minify more stuffs

--- public/js/syntax-highlighter/scripts/shBrushPlain.js
+++ public/js/syntax-highlighter/scripts/shBrushPlain.js
@@ -1,11 +1,1 @@
-;(function()
-{
-	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
-	function Brush()
-	{
-	};
-	Brush.prototype	= new SyntaxHighlighter.Highlighter();
-	Brush.aliases	= ['text', 'plain'];
-	SyntaxHighlighter.brushes.Plain = Brush;
-	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
-})();
+!function(){function e(){}"undefined"!=typeof require&&(SyntaxHighlighter=require("shCore").SyntaxHighlighter),e.prototype=new SyntaxHighlighter.Highlighter,e.aliases=["text","plain"],SyntaxHighlighter.brushes.Plain=e,"undefined"!=typeof exports&&(exports.Brush=e)}();
