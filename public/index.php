6f723d8d9a07f50dfe9ec4b4691f3035eb593624
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-05-22 13:34:33 -0500

    even more testing

--- public/index.php
+++ public/index.php
@@ -4,14 +4,16 @@ $origin = "https://map2.pl3x.net";
 $og_title = "Pl3xMap";
 $og_url = "https://modrinth.com/mod/pl3xmap";
 $og_image = $origin . "/images/og_large.png";
+$og_large_image = true;
 $og_desc = "Pl3xMap is a minimalistic and lightweight world map viewer for Minecraft servers using the vanilla Minecraft rendering style";
 $og_keywords = "Minecraft, Mod, Plugin, Map, Pl3x, Pl3xMap, Dynamic, Live, LiveMap, BlueMap, Dynmap, SquareMap, Bukkit, BukkitMC, Spigot, SpigotMC, Paper, PaperMC, Purpur, PurpurMC, Fabric, FabricMC, Forge, ForgeMC, Quilt, QuiltMC";
 
 function setOg($id, $title, $desc) {
-    global $origin, $og_title, $og_url, $og_image, $og_desc;
+    global $origin, $og_title, $og_url, $og_image, $og_large_image, $og_desc;
     $og_title .= " - " . $title;
     $og_url = $origin . "/" . $id;
     $og_image = $origin . "/images/og_small.png";
+    $og_large_image = false;
     $og_desc = $desc;
 }
 
@@ -94,6 +96,7 @@ if (isset($id)) {
     <meta property="og:title" content="<?=$og_title?>">
     <meta property="og:description" content="<?=$og_desc?>">
     <?php if (isset($og_image)) {?><meta property="og:image" content="<?=$og_image?>"><?php } ?>
+    <?php if ($og_large_image) {?><meta name="twitter:card" content="summary_large_image"><?php } ?>
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
