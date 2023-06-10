575fd58c810b9729d8f893de7451b02b28adad0f
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-06-10 13:17:37 -0500

    more admin progress

--- public/admin.php
+++ public/admin.php
@@ -12,45 +12,56 @@ if (!$logged_in) {
 $isAdmin = true;
 $og['title'] .= ' - Admin';
 
+if (count($_POST) > 0) {
+  var_dump($_POST);
+  header('HTTP/1.1 303 See Other');
+  header('location: /admin');
+  die();
+}
+
 require_once(__DIR__ . '/php/header.php');
 ?>
       <dialog id="d0">
         <form method="post" action="/admin">
-          <label><span>Slug:</span><input type="text" name="" autocomplete="off"></label>
-          <label><span>Title:</span><input type="text" name="" autocomplete="off"></label>
-          <label><span>Desc:</span><input type="text" name="" autocomplete="off"></label>
-          <label><span>Content:</span><div><textarea></textarea></div></label>
+          <input type="hidden" id="isNew" name="isNew" value="false">
+          <input type="hidden" id="pageId" name="pageId" value="-1">
+          <label><span>Slug:</span><input type="text" id="slug" name="slug" autocomplete="off"></label>
+          <label><span>Title:</span><input type="text" id="title" name="title" autocomplete="off"></label>
+          <label><span>Desc:</span><input type="text" id="description" name="description" autocomplete="off"></label>
+          <label><span>Content:</span><div><textarea id="content" name="content"></textarea></div></label>
           <div class="buttons"><button id="cancelBtn"><span>Cancel</span></button><button id="saveBtn"><span>Save</span></button></div>
         </form>
         <svg viewBox="0 0 32 32"><path stroke="#000000" stroke-width="2" stroke-linecap="round" d="M1 30 30 1M16 30 30 16"/></svg>
       </dialog>
       <div class="content">
         <div class="buttons"><div></div><button id="addBtn"><svg viewBox="0 0 1024 1024"><path d="M467.627 468H235.489c-24.3 0-44 19.7-44 44s19.7 44 44 44h232.138v232.138c0 24.3 19.7 44 44 44s44-19.7 44-44V556h232.138c24.3 0 44-19.7 44-44s-19.7-44-44-44H555.627V235.862c0-24.3-19.7-44-44-44s-44 19.7-44 44V468zM512 1024C229.23 1024 0 794.77 0 512S229.23 0 512 0s512 229.23 512 512-229.23 512-512 512z"/></svg><span>Add New</span></button></div>
+        <ul>
 <?php
-echo '        <ul>' . "\n";
 foreach ($sections as $section) {
 ?>
           <li class="page_entry" id="<?=$section['slug']?>">
             <div><svg viewBox="0 0 24 24"><path d="M3 15h18M3 9h18" stroke="#000" stroke-width="2" stroke-linecap="round"/></svg></div>
+            <div style='display:none'><?=$section['id']?></div>
+            <div style='display:none'><?=$section['content']?></div>
             <div><?=$section['slug']?></div>
             <div><?=$section['title']?></div>
             <div><?=$section['description']?></div>
           </li>
 <?php
 }
-echo '        </ul>' . "\n";
-echo '      </div>' . "\n";
-
+?>
+        </ul>
+      </div>
+<?php
 require_once(__DIR__ . '/php/footer.php');
-
-echo  '  <script>';
+?>
+  <script><?php
 ob_start('minify_js');
 echo 'setTimeout(() => go(window.location.pathname.split(\'/admin/\')[1], \'admin\'), 0);';
 echo 'const logged_in = ' . ($logged_in ? 'true' : 'false') . ';';
 require_once(__DIR__ . '/js/admin.js');
 require_once(__DIR__ . '/js/common.js');
 ob_end_flush();
-echo '</script>' . "\n";
-?>
+?></script>
 </body>
 </html>
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,18 +1,40 @@
 const addBtn = document.querySelector('#addBtn');
 const cancelBtn = document.querySelector('#cancelBtn');
-const saveBtn = document.querySelector('#saveBtn');
+const isNew = document.querySelector('#isNew');
 const d0 = document.querySelector('#d0');
+const pageId = document.querySelector('#pageId');
+const slug = document.querySelector('#slug');
+const title = document.querySelector('#title');
+const description = document.querySelector('#description');
+const content = document.querySelector('#content');
 
 addBtn.onclick = (e) => {
-    e.preventDefault();
-    addBtn.blur();
-    openDialog(d0)
+  e.preventDefault();
+  addBtn.blur();
+  pageId.value = -1;
+  slug.value = '';
+  title.value = '';
+  description.value = '';
+  content.value = '';
+  isNew.value = true;
+  openDialog(d0)
 };
 
 cancelBtn.onclick = (e) => {
-    e.preventDefault();
-    cancelBtn.blur();
-    closeDialog(d0)
+  e.preventDefault();
+  cancelBtn.blur();
+  closeDialog(d0)
 };
 
-//openDialog(d0);
+const entries = document.querySelectorAll('.page_entry');
+entries.forEach(entry => {
+  entry.onclick = (e) => {
+    pageId.value = entry.children[1].innerHTML;
+    slug.value = entry.children[3].innerHTML;
+    title.value = entry.children[4].innerHTML;
+    description.value = entry.children[5].innerHTML;
+    content.value = entry.children[2].innerHTML;
+    isNew.value = false;
+    openDialog(d0)
+  }
+});
